55d56b761fe85d4a9892480c0193c6e84baa5f9f
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-05-26 21:25:21 +0530

    Initial Commit

---
+++ DataType1.py
@@ -0,0 +1,39 @@
+# Integer
+print("*****************************   INTEGER   ************************************")
+var1 = 5
+var2 = 2
+print("Var1 is of type ", type(var1))
+print("Integer addition = ", var1 + var2)
+print("Integer subtraction = ", var1 - var2)
+print("Integer multiplication = ", var1 * var2)
+print("Integer division = ", var1 / var2)
+print("Integer absolute division = ", var1 // var2)
+print()
+
+# Float
+print("*****************************   FLOAT   ************************************")
+float1 = 5.0
+float2 = 2.0
+print("float1 is of type ", type(float1))
+print("Float addition = ", float1 + float2)
+print("Float subtraction = ", float1 - float2)
+print("Float multiplication = ", float1 * float2)
+print("Float division = ", float1 / float2)
+print("Float absolute division = ", float1 // float2)
+print()
+
+# Complex
+print("*****************************   COMPLEX   ************************************")
+
+complex1 = 3 + 4j
+complex2 = 3 - 4j
+print("complex1 is of type ", type(complex1))
+print("Complex addition = ", complex1 + complex2)
+print("Complex subtraction = ", complex1 - complex2)
+print("Complex multiplication  = ", complex1 * complex2)
+print("Complex division = ", complex1 / complex2)
+print("complex1 conjugate = ", complex1.conjugate())
+print("complex2 conjugate = ", complex2.conjugate())
+print("Real part of complex2 = ", complex2.real)
+print("Imaginary part of complex2 = ", complex2.imag)
+print()
