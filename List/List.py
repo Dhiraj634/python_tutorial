77ab393c2d6f7247c52bbb9e7bdfb3956f8e0b7c
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-05-30 11:24:34 +0530

    list

--- List/List.py
+++ List/List.py
@@ -1,8 +1,11 @@
 marks = [8, 7, 5, 6, 6, 9, 7, 8]
 length = len(marks)  # get the length of the list
-
+print()
+print("*" * 70)
+print()
 # "in" keyword can be used to search in the list
 
+print("marks = ", marks)
 if 9 in marks:
     print("9 is present in the list")
 if 4 not in marks:
@@ -11,24 +14,30 @@ if 4 not in marks:
 # indexing
 # using index to access the element of list
 # list indexing always starts with 0 i.e. first element is at index 0
-print(marks[0])
-print(marks[1])
+print("marks[0] = ", marks[0])
+print("marks[1] = ", marks[1])
 
-# count method in list
-count_of_six = marks.count(6)
-print(count_of_six)
+# Slicing
+
+print("slice 1 = ", marks[1:5])
+print("slice 2 = ", marks[:5])
+print("slice 3 = ", marks[1:])
+print("slice 4 = ", marks[::1])
+
 
 # + and * operators
-print([1, 2] + [3, 4, 5])
-print([1, 2] * 3)
-print([0] * 5)
+print("+ Operator = ", [1, 2] + [3, 4, 5])
+print("* Operator = ", [1, 2] * 3)
+print("Creating array of zeros = ", [0] * 5)
 
 # looping in list
+print("First Loop : ", end="")
 for i in range(len(marks)):
     print(marks[i], end=" ")
 
 print()
 
+print("Second Loop : ", end="")
 for mark in marks:
     print(mark, end=" ")
 
@@ -37,34 +46,40 @@ print()
 # Built-in functions
 
 length = len(marks)
+print("Length of marks = ", length)
 total = sum(marks)
+print("Sum of marks = ", total)
 minimum = min(marks)
+print("minimum of marks = ", minimum)
 maximum = max(marks)
+print("maximum of marks = ", maximum)
 # average = sum(marks)/len(marks)
 
 # List methods
 
+print("marks = ", marks)
+
 marks.append(10)
-print(marks)
+print("After append = ", marks)
 
 marks.sort()
-print(marks)
+print("after sort = ", marks)
 
 print("count of 6 ", marks.count(6))
 
 print("first occurance of 6 ", marks.index(6))
 
 marks.reverse()
-print(marks)
+print("after reverse = ", marks)
 
 marks.remove(6)
-print(marks)
+print("after remove = ", marks)
 
 marks.pop(2)
-print(marks)
+print("after pop = ", marks)
 
 marks.insert(2, 12)
-print(marks)
+print("after insert = ", marks)
 
 # copy list
 copied_marks = marks[:]
--- List/More_on_list.py
+++ List/More_on_list.py
@@ -5,29 +5,29 @@ from random import choice, sample, shuffle
 names = ["Naruto", "Kakashi", "Sasuke", "Sakura"]
 
 name = choice(names)  # Picks up random item from list
-print("Randomly selected name is ", name)
+print("Randomly selected name is = ", name)
 
 sample_name = sample(names, 2)  # picks a group of n random items from names
-print(sample_name)
+print("Sample Name selected is = ", sample_name)
 
 shuffle(names)  # The shuffle function modifies the original list
-print(names)
+print("shuffled names = ", names)
 
 # List Comprehension
 
 # creating a list of first 10 positive number
 list1 = [i for i in range(1, 11)]
-print(list1)
+print("list1 = ", list1)
 
 # creating a list of square of first 10 positive numbers
 list2 = [i**2 for i in range(1, 11)]
-print(list2)
+print("list2 = ", list2)
 
 # creating a list of even numbers below 20
 # [i for i in range(2,21,2)] is also an approach
 list3 = [i for i in range(1, 21) if i % 2 == 0]
-print(list3)
+print("list3 = ", list3)
 
 # creating a 2-D list
 list4 = [[i, j] for i in range(3) for j in range(3)]
-print(list4)
+print("list4 = ", list4)
